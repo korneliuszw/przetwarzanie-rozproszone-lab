d8eb5c3017064460827d50828384ac05df56f9fd
Grzegorz Koszczał <grzkoszczal@task.gda.pl>
2025-03-11 10:11:28 +0100

    added imports and test cases to main

---
+++ lab2/dynamic/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "stack.h"
+#include "utils.h"
+
+int main() {
+    // initialize the stack as empty
+    StackNode* stack = NULL;
+
+    // some 'test cases' to check functionality
+    // pushing some values onto the stack
+    push(&stack, 10);
+    push(&stack, 40);
+    push(&stack, 55);
+    print_stack(stack);
+    
+    // popping values from the stack
+    pop(&stack);
+    print_stack(stack);
+    pop(&stack);
+    print_stack(stack);
+    pop(&stack);
+    print_stack(stack);
+
+    return 0;
+}
