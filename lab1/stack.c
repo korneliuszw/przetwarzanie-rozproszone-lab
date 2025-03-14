4d146d2e4a1befb67fed2610c98ca1f207dc3f73
korneliuszw <korneliuszWStudio@gmail.com>
2025-03-14 12:25:01 +0100

    Lab 1 - BST

--- lab1/stack.c
+++ lab1/stack.c
@@ -1,76 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-/*
-    !!! IMPORTANT !!!
-    How to compile and execute:
-    gcc -Wall -fsanitize=leak -pedantic -o stack stack.c && ./stack
-*/
+typedef struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
 
-/*
-    TODO:
-    1. - define a struct for the stack node
-    2. - define function to push a value onto the stack
-    3. - define function to pop last value from top of the stack
-    4. - test how the stack performs on the test cases (You can provide your own)
-*/
+typedef struct {
+    TreeNode* root;
+} BinaryTree;
 
-typedef struct StackNode {
-    // here define data stored and pointer to next node
-
-} StackNode;
-
-void push(StackNode** top, int value) {
-    /*
-        TODO:
-        1. - create new stack node (and allocate memory using malloc())
-        2. - check if new stack node is NULL, if yes (malloc fail), return error
-        3. - set node data
-        4. - make pointer defined in StackNode point to the previous top
-        5. - assign the top pointer to the new node
-    */
-
+bool is_leaf(TreeNode* node) {
+    return node == NULL || (node->left == NULL && node->right == NULL);
 }
 
-int pop(StackNode** top) {
-    /*
-        TODO:
-        1. - check if top of the stack is NULL, if yes, return
-        2. - pop the top node and return its data
-        3. - move the top pointer (store it in temporary data) and retrieve its data
-        4. - free the memory of the popped node (use free())
-    */
-
+TreeNode* tree_node_init(int data) {
+    TreeNode* new_node = malloc(sizeof(TreeNode));
+    new_node->left = new_node->right = NULL;
+    new_node->data = data;
+    return new_node;
 }
 
-void print_stack(StackNode* top) {
-    /*
-        TODO:
-        1. check if top of the stack is empty, if yes, return
-        2. write 'while' loop that will print all values from top to bottom
-    */
-
+TreeNode** tree_find_node(BinaryTree* tree, int data, bool skip_existing) {
+    TreeNode** current = &tree->root;
+    while (*current != NULL) {
+        if (data < (*current)->data) {
+            current = &(*current)->left;
+        }  else if (data > (*current)->data) {
+            current = &(*current)->right;
+        } else if (skip_existing) return NULL;
+        else return current;
+    }
+    return current;
 }
 
+void tree_insert(BinaryTree* root, int data) {
+    if (root->root == NULL) {
+        root->root = tree_node_init(data);
+        return;
+    }
+    TreeNode** node = tree_find_node(root, data, true);
+    if (node != NULL && *node == NULL) {
+        *node = tree_node_init(data);
+    }
+}
+
+TreeNode** node_successor(TreeNode* node) {
+    TreeNode** current = &node->right;
+    while (*current != NULL && (*current)->left != NULL) {
+        current = &(*current)->left;
+    }
+    return current;
+}
+
+void tree_pop(BinaryTree* root, int data) {
+    TreeNode** node = tree_find_node(root, data, false);
+    if (node == NULL || *node == NULL) return;
+    if ((*node)->left == NULL && (*node)->right == NULL) {
+        free(*node);
+        *node = NULL;
+    } else if ((*node)->left == NULL || (*node)->right == NULL) {
+        // one child
+        TreeNode* new_node;
+        if ((*node)->left == NULL) new_node = (*node)->right;
+        else new_node = (*node)->left;
+        free(*node);
+        *node = new_node;
+    } else {
+        // two children
+        TreeNode** succ = node_successor(*node);
+        (*node)->data = (*succ)->data;
+        free(*succ);
+        *succ = NULL;
+    }
+}
+
+void tree_print(TreeNode* node) {
+    if (node == NULL) return;
+    tree_print(node->left);
+    printf(" %d ", node->data);
+    tree_print(node->right);
+}
+
+BinaryTree* tree_init() {
+    BinaryTree* tree = malloc(sizeof(BinaryTree));
+    return tree;
+}
+
+
+void node_destroy(TreeNode* node) {
+    if (node == NULL) return;
+    node_destroy(node->left);
+    node_destroy(node->right);
+    free(node);
+}
+void tree_destroy(BinaryTree* tree) {
+    if (tree->root != NULL) {
+        node_destroy(tree->root);
+    }
+    free(tree);
+}
 
 int main() {
-    // initialize the stack as empty
-    StackNode* stack = NULL;
-    
-    // some 'test cases' to check functionality
-    // pushing some values onto the stack
-    push(&stack, 10);
-    push(&stack, 20);
-    push(&stack, 30);
-    print_stack(stack);
-    
-    // popping values from the stack
-    pop(&stack);
-    print_stack(stack);
-    pop(&stack);
-    print_stack(stack);
-    pop(&stack);
-    print_stack(stack);
-
+    BinaryTree* tree = tree_init();
+    tree_insert(tree, 10);
+    tree_insert(tree, 5);
+    tree_insert(tree, 5);
+    tree_insert(tree, 12);
+    tree_insert(tree, 11);
+    tree_insert(tree, 13);
+    tree_insert(tree, 4);
+    tree_insert(tree, 6);
+    tree_insert(tree, 8);
+    tree_insert(tree, 7);
+    tree_print(tree->root);
+    tree_pop(tree, 10);
+    printf("\n");
+    tree_print(tree->root);
+    tree_destroy(tree);
     return 0;
 }
